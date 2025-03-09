37793cb9e84a533194e80711a716618ae41835d3
Neil Chauhan <neilchauhan2@gmail.com>
2025-03-09 19:44:19 +0530

    Time: 39 ms (90.4%), Space: 53.2 MB (22.65%) - LeetHub

---
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+    const temp = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        temp.push(fn(arr[i], i));
+    }
+
+    return temp;
+};
