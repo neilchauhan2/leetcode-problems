7d2e776ed67e4aaf86c48372e39c1c637ed84c3f
Neil Chauhan <neilchauhan2@gmail.com>
2025-03-09 19:38:04 +0530

    Time: 65 ms (18.54%), Space: 56.3 MB (22.95%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,28 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let initial = init;
+    return {
+        increment: function() {
+            initial += 1;
+            return initial;
+        },
+        decrement: function() {
+            initial -= 1;
+            return initial;
+        },
+        reset: function() {
+            initial = init;
+            return initial;
+        }
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
